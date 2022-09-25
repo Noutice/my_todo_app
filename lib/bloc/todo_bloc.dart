43f6d57c078121f27053ef9053ae04676f8c8647
Noutice <hrachavoyan9@gmail.com>
2022-09-26 00:05:10 +0400

    .

--- lib/bloc/todo_bloc.dart
+++ lib/bloc/todo_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/functions/text_function.dart';
 
 part 'todo_event.dart';
 part 'todo_state.dart';
@@ -7,18 +8,7 @@ part 'todo_state.dart';
 class TodoBloc extends Bloc<TodoEvent, TodoState> {
   TodoBloc() : super(TodoState()) {
     on<AddListEvent>((AddListEvent event, Emitter<TodoState> emit) {
-      int k = 0;
-      bool x = true;
-      for (int i = 0; i < event.controller.text.length; i++) {
-        if (event.controller.text[i] == ' ') {
-          x = false;
-          k = i;
-        } else {
-          break;
-        }
-      }
-      String text = event.controller.text
-          .substring(x ? k : k + 1, event.controller.text.length);
+      String text = textFunction(event.controller.text);
       if (state.todoList.length == 5 || text.isEmpty) return;
       emit(
         state.changeWith(
